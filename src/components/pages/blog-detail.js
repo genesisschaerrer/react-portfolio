81ded0a5a13a42e1855261b0bd0c7805cb881d26
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-01-12 15:52:20 -0800

    Creating a Dedicated Blog Item Component

---
+++ src/components/pages/blog-detail.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react"
+
+export default class BlogDetail extends Component {
+    render(){
+        return (
+          <h1>Blog Detail</h1>  
+        )
+    }
+}
