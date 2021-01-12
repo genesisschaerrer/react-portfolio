c2ea388e6d5115a594ec674a093ae613d10b3fc4
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-01-12 15:22:40 -0800

    calling the blog api

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,7 +1,38 @@
-import React from "react"
+import React, { Component } from "react"
 import {Link} from "react-router-dom"
+import axios from "axios"
 
-export default function (){
+
+
+export default class Blog extends Component{
+    constructor(){
+        super()
+
+        this.state = {
+            blogItems: []
+        }
+    }
+
+    getBlogItems(){
+        axios
+        .get("https://genesisschaerrer.devcamp.space/portfolio/portfolio_blogs",
+         {withCredentials: true})
+         .then(response => {
+             this.setState({
+                 blogItems: response.data.portfolio_blogs
+             })
+         })
+         .catch(error => {
+             console.log("getBlogItems error", error)
+         })
+         
+    }
+
+    componentDidMount(){
+        this.getBlogItems()
+    }
+
+    render (){
     return (
     <div>
         <h2>Blog</h2>
@@ -9,5 +40,5 @@ export default function (){
             <Link to="/about-me">Read more about me</Link>
         </div>
     </div>
-    )
+    )}
 }
