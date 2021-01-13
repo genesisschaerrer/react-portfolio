7315d608045a224e9b469c7f459215594061f4b8
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-01-13 15:20:25 -0700

    infinite scroll

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,79 +1,82 @@
-import React, { Component } from "react"
-import {Link} from "react-router-dom"
-import axios from "axios"
-import BlogItem from "../blog/blog-item"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+import BlogItem from "../blog/blog-item";
+class Blog extends Component {
+  constructor() {
+    super();
+    this.state = {
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0,
+      isLoading: true,
+    };
+    this.getBlogItems = this.getBlogItems.bind(this);
+    this.activateInfiniteScroll();
+  }
 
+  activateInfiniteScroll() {
+    window.onscroll = () => {
+      if (
+        this.state.isLoading ||
+        this.state.blogItems.length === this.state.totalCount
+      ) { return;
+      }
 
+      if (
+        Math.ceil(window.innerHeight + document.documentElement.scrollTop) ===
+        document.documentElement.offsetHeight
+      ) {
+        this.getBlogItems();
+        // console.log("get more posts");
+      }
+    };
+  }
 
-export default class Blog extends Component{
-    constructor(){
-        super()
+  getBlogItems() {
+    this.setState({
+      currentPage: this.state.currentPage + 1,
+    });
 
-        this.state = {
-            blogItems: [],
-            totalCount: 0,
-            currentPage: 0, 
-            isLoading: true
+    axios
+      .get(
+        `https://genesisschaerrer.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`,
+        {
+          withCredentials: true,
         }
-
-        this.activateInfiniteScroll()
-    }
-
-    activateInfiniteScroll = () => {
-        window.onscroll = () => {
-            if( this.state.isLoading ||
-                this.state.blogItems.length === this.state.totalCount){
-                return
-            }
-
-            if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight){
-                this.getBlogItems()
-            } 
-        }
-        
-    }
-
-    getBlogItems(){
+      )
+      .then((response) => {
+        console.log("getting", response.data);
         this.setState({
-            currentPage: this.state.currentPage + 1
-        })
+          blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
+          totalCount: response.data.meta.total_records,
+          isLoading: false,
+        });
+      })
+      .catch((error) => {
+        console.log("getBlogItem error", error);
+      });
+  }
 
-        axios
-        .get(`https://genesisschaerrer.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`,
-         {withCredentials: true})
-         .then(response => {
-            console.log("getting", response.data)
-             this.setState({
-                 blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
-                 totalCount: response.data.meta.total_records,
-                 isLoading: false
-             })
-         })
-         .catch(error => {
-             console.log("getBlogItems error", error)
-         })
-         
-    }
+  componentWillMount() {
+    this.getBlogItems();
+  }
 
-    componentDidMount(){
-        this.getBlogItems()
-    }
-
-    render (){
-        const blogRecords = this.state.blogItems.map(blogItem => {
-            return <BlogItem key={blogItem.id} blogItem={blogItem} /> 
-        })
-
+  render() {
+    const blogRecords = this.state.blogItems.map((blogItem) => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+    });
     return (
-        <div className="blog-container">
-            <div className="content-container">{blogRecords}</div>
-
-            {this.state.isLoading? (
-            <div className="content-loader">
-                <FontAwesomeIcon icon="spinner" spin/>
-            </div>)
-            : null}
-        </div>
-    )}
+      <div className="blog-container">
+        <div className="content-container">{blogRecords}</div>
+        {this.state.isLoading ? (
+          <div className="content-loader">
+            <FontAwesomeIcon icon="spinner" spin />
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
+export default Blog;
